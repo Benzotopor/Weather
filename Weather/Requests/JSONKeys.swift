60c40e86570bd6c6c07cb10e02550856b076a185
Nusratbek <nusratbekusmanov@gmail.com>
2018-12-03 02:51:57 +0300

    Add missing files

---
+++ Weather/Requests/JSONKeys.swift
@@ -0,0 +1,23 @@
+//
+//  JSONKeys.swift
+//  Weather
+//
+//  Created by Nusratbek Usmanov on 29.11.2018.
+//  Copyright © 2018 Nusratbek. All rights reserved.
+//
+
+import UIKit
+
+class JSONKeys {
+    static let coord        = "coord"
+    static let lat          = "lat"
+    static let lon          = "lon"
+    static let weather      = "weather"
+    static let icon         = "icon"
+    static let main         = "main"
+    static let temp         = "temp"
+    static let id           = "id"
+    static let name         = "name"
+    static let message      = "message"
+    static let list         = "list"
+}
